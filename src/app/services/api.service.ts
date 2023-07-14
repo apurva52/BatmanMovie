ed928daf062aae208e8aa2b414d851e8dc03d055
apurva52 <55528747+apurva52@users.noreply.github.com>
2023-07-14 20:20:20 +0530

    Create api.service.ts

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { batManMovies } from '../app/models/data-interface';
+
+@Injectable(
+)
+export class AppService {
+  constructor(private http:HttpClient){}
+  batmanMovieUrl= 'https://www.omdbapi.com/?s=Batman&apikey=b45f3dce';
+
+  getBatmanMovieList()
+ {
+  let dataUrl: string = `${this.batmanMovieUrl}`
+  return this.http.get<batManMovies>(dataUrl).pipe(catchError(this.handleError))
+ }
+
+ public handleError(error: HttpErrorResponse) {
+  let errorMessage: string = ''
+  if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error:${error.message}`
+  }
+  else {
+      errorMessage = `Status: ${error.status} \n Message:${error.message}`;
+  }
+  return throwError(errorMessage)
+}
+}
