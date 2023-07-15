c6ea42e5ea9b038d6ddfd5520fc4b4ab9daaae23
apurva52 <55528747+apurva52@users.noreply.github.com>
2023-07-15 12:12:02 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from './services/api-service';
-import { movieListEntry, batManMovies } from './models/data-interface.ts';
+import { movieListEntry, batManMovies } from './models/data-interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
