7eef8229ab97580a756082960be8a9706e6df9f9
apurva52 <55528747+apurva52@users.noreply.github.com>
2023-07-18 13:43:59 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef,AfterViewInit } from '@angular/core';
 import { ApiService } from './services/api.service';
 import { movieListEntry, batManMovies } from './models/data-interface';
 @Component({
@@ -6,28 +6,70 @@ import { movieListEntry, batManMovies } from './models/data-interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   public movieList: movieListEntry[] = [];
+  @ViewChild('btn1') btn1: ElementRef; 
   copyData: movieListEntry[] = [];
   search: string;
   constructor(private apiservice: ApiService) {
     this.fetchMovieList();
+    
+  }
+  ngAfterViewInit(){
+    this.btn1.nativeElement.style.backgroundColor = '#164971'
+    this.btn1.nativeElement.style.color = 'white'
   }
   fetchMovieList() {
     this.apiservice.getBatmanMovieList().subscribe((res: batManMovies) => {
-      this.movieList = res.Search;
+      this.apiservice.getMovieDetails(res.Search).subscribe((dt: movieListEntry[]) => {
+      this.movieList = dt;
+      console.log(this.movieList)
       this.copyData = [...this.movieList];
       console.log(res);
+      })
     });
   }
 
-  Keyup(): void {
-    console.log("errer");
-    this.movieList = this.copyData.filter(res => {
-      if (res.Year.toLowerCase().includes(this.search.toLowerCase().slice(0)))
+  Yearwisemovie(dateval: string, btn: HTMLElement, btnlist: HTMLElement[])
+  {
+    this.removeEffect(btnlist)
+    console.log(btn)
+    btn.style.backgroundColor ="#164971"
+     btn.style.color = "white"
+    this.movieList.map(dt => {
+      if (dt.Year.includes('–'))
       {
-        return true;
+        dt.Year = dt.Year.split('–')[1]
       }
-    });
+     return dt;
+  })
+
+    switch (dateval){
+      case 'All':
+        this.movieList = this.copyData;
+        break;
+      case '1980':
+        this.movieList = this.copyData.filter(res => Number(res.Year) > 1980 && Number(res.Year)  <= 1990)
+        break;
+      case '1990':
+        this.movieList = this.copyData.filter(res => Number(res.Year) > 1990 && Number(res.Year) <= 2000);
+        break;
+      case '2000':
+        this.movieList = this.copyData.filter(res => Number(res.Year) > 2000 && Number(res.Year) <= 2010);
+        break;
+      case '2010':
+        this.movieList = this.copyData.filter(res => Number(res.Year) > 2010 && Number(res.Year) <= 2020);
+        break;
+      case '2020':
+        this.movieList = this.copyData.filter(res => Number(res.Year) > 2020 );
+        break;
+    }
+  }
+  removeEffect(btnlist):void{
+    for (let k of btnlist){
+      k.style.backgroundColor = ""
+      k.style.color = ""
+    }
+
   }
 }
